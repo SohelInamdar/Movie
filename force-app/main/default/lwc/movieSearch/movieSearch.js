86fad2dbbb85af3c206c2f1f2e8bdd2ba8516493
Sohel Inamdar <sohail17inamdar@gmail.com>
2025-03-07 05:51:48 +0530

    Modified files

--- force-app/main/default/lwc/movieSearch/movieSearch.js
+++ force-app/main/default/lwc/movieSearch/movieSearch.js
@@ -8,6 +8,7 @@ export default class MovieSearch extends LightningElement {
     dataRecieved = [];
     showTileDisplaySection = false
     timeoutForCall;
+    selectedMovie = '';
 
     eventHandler(event){
         let {name ,value} = event.target;
@@ -41,7 +42,6 @@ export default class MovieSearch extends LightningElement {
             // this.dataRecieved = data.Search;
             this.showTileDisplaySection = true;
             this.dataRecieved = [{"Title":"Captain America: The First Avenger","Year":"2011","imdbID":"tt0458339","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BNzUyM2YyY2MtNzNlMS00MWU5LTgxNjAtNzZlNmI2NjU2NDZlXkEyXkFqcGc@._V1_SX300.jpg"},
-                {"Title":"The Toxic Avenger","Year":"1984","imdbID":"tt0090190","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BNDZjMGZjMDktOTZlNC00MTQ5LTkzY2UtZjM1MjE0NDcwNzk1XkEyXkFqcGc@._V1_SX300.jpg"},
                 {"Title":"The Toxic Avenger Part II","Year":"1989","imdbID":"tt0098503","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BYzZkNDhlMzUtMzc3Zi00MTJhLWFhMjEtN2IyOTM3NTZlODllXkEyXkFqcGc@._V1_SX300.jpg"},
                 {"Title":"Citizen Toxie: The Toxic Avenger IV","Year":"2000","imdbID":"tt0212879","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BOWU5NzE2ZjAtNjU5Yy00MTlmLWEyNzctYTA3NjNlNDFiMzVlXkEyXkFqcGc@._V1_SX300.jpg"},
                 {"Title":"The Toxic Avenger Part III: The Last Temptation of Toxie","Year":"1989","imdbID":"tt0098502","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BNmUxMDQ1ZjYtNDY3ZS00MzRjLWJhODYtMDY3ZGM1YWFkNmM3XkEyXkFqcGc@._V1_SX300.jpg"},
@@ -52,7 +52,7 @@ export default class MovieSearch extends LightningElement {
                 {"Title":"The Avenger","Year":"1962","imdbID":"tt0056174","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMWJhYTYzZTAtMDMxYi00NDhjLTlkYzItYTQwNmI3M2NkYWIxXkEyXkFqcGc@._V1_SX300.jpg"}];
 
             // console.log('data search :', data.Search)
-            console.log('data :', data)
+            // console.log('data :', data)
             console.log('data recived :', this.dataRecieved);   
     }
 
@@ -64,6 +64,12 @@ export default class MovieSearch extends LightningElement {
             { label: 'EPISODE', value: 'episode' }
         ];
     }
+
+    selectedMovieHandler(event){
+        this.selectedMovie = event.detail;
+    }
+
+    
 
     
 }
--- force-app/main/default/lwc/movieTile/movieTile.js
+++ force-app/main/default/lwc/movieTile/movieTile.js
@@ -3,6 +3,14 @@ import { LightningElement,api } from 'lwc';
 export default class MovieTile extends LightningElement {
     @api movie;
     @api showTile = false;
+    @api selectedMovieId;
     
-    
+    onclickHandler(event){
+
+        this.dispatchEvent(new CustomEvent('send',{detail: this.movie.imdbID}));
+    }
+
+    get getSeletedTile(){
+        return this.selectedMovieId === this.movie.imdbID ?  'selectedTile' : 'outerTile';
+    }
 }
